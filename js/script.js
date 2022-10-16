9b69045b24111f893176223a354e28896a602638
sL6H507 <62222489+sL6H507@users.noreply.github.com>
2022-10-16 19:43:34 +0300

    Final Version
    iHOPE :)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
-
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
